6b510da1a2d2620d06f3a38e54680723dca18e57
zps <kshah215@gmail.com>
2013-12-24 20:45:24 +0530

    Complete work! Please press tab for every textbox in form :)

--- backend.php
+++ backend.php
@@ -1,3 +1,21 @@
 <?php
-
+$language=$_POST['language'];
+$num_var=$_POST['numArg'];
+$variables=$_POST['var_name'];
+$type=$_POST['type'];
+$functionName=$_POST['functionName'];
+$dim=$_POST['dim'];
+if($language=='c')
+{
+	$i=0;
+	for($i=0;$i<$num_var;$i++)
+	{
+		echo $type[$i].' '.$variables[$i].', ';
+		
+	}
+}
+else if($language=='java')
+{
+	$className=$_POST['className'];
+}
 ?>
