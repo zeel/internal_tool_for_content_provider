2bff86afd79adc71a05faadabde3e9f1e152250b
unknown <kshah215@gmail.com>
2013-12-31 16:36:46 +0530

    added functionality of function suggestion

--- backend.php
+++ backend.php
@@ -4,8 +4,6 @@ header('Content-Type: application/json');
 $language=json_decode($_POST['language']);
 @$className=$_POST['className'];
 $functionName=$_POST['functionName'];
-$functionReturnType=$_POST['functionReturnType'];
-$functionReturnDimension=$_POST['functionReturnDimension'];
 $file = 'db.json';
 $string = file_get_contents("db.json") or die('Could not read database!');
 $php_array=json_decode($string,true);
@@ -18,18 +16,21 @@ for($i;$i<count($php_array['language']);$i++)
 	if($php_array['language'][$i]['name']==$language[$j])
 	{
 		$variables=json_decode($_POST[$language[$i].'variables']);
+		$functionReturn=$_POST[$language[$i].'functionReturnSuggestion'];
 		$variable_string=implode(", ", $variables);
 		$fuctionSignature=$php_array['language'][$i]['fuctionSignature'];
+		$fuctionSignature = str_replace("functionReturn", $functionReturn, $fuctionSignature);
 		$fuctionSignature = str_replace("functionName", $functionName, $fuctionSignature);
-		for($k=0;$k<count($php_array['language'][$i]["type"]);$k++)
+		/*for($k=0;$k<count($php_array['language'][$i]["type"]);$k++)
 		{
 			if($php_array['language'][$i]["type"][$k]["name"]==$functionReturnType)
 			{
 				$functionReturn=str_replace("test",'',$php_array['language'][$i]["type"][$k]['dimension'][$functionReturnDimension]["suggestion"][0]);
 				$fuctionSignature = str_replace("functionReturn", $functionReturn, $fuctionSignature);
 			}
-		}
-		 $str=$variable_string;
+		}*/
+		
+		$str=$variable_string;
 		$fuctionSignature = str_replace("variables",$str, $fuctionSignature);	
 		if(!is_null($className))
 		$fuctionSignature = str_replace("className", $className, $fuctionSignature);
